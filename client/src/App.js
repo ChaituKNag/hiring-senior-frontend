fd3cffaf00373b66e3c3cf8d8f9499bae47a8704
nkonada <knc@outlook.in>
2020-07-10 23:07:19 +0530

    routing added at the app level

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import Header from "./components/common/Header";
+import InvoiceList from "./components/InvoiceList";
+import NewInvoice from "./components/NewInvoice";
 
 function App() {
-  return <Container>Hello World</Container>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/home">
+          <InvoiceList />
+        </Route>
+        <Route path="/new-invoice">
+          <NewInvoice />
+        </Route>
+        <Route path="/">
+          <Redirect to="/home" />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
